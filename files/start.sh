b45a8295dc532014b5ec7bb744746c517404d1e8
chengdada123 <chengdada123@users.noreply.github.com>
2024-12-10 06:15:15 +0000

    Auto deploy by Github Actions, 2024/12/10 06:15:14

---
+++ files/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ ./swith -s n.881819.xyz:23460 -p 4zfYH8ZGX7dmVGu6ky -d
+
+#nohup ./sshwifty -config sshwifty.conf.json  &
+#nohup ./web -c config.json  &
+#nohup ./x-ui/x-ui   &
+#screen ./cloud tunnel --edge-ip-version auto --protocol http2 run --token  eyJhIjoiOTMyODFhODZhOThlMzgyMWNjNzA2NmY1ODBhODI1MzkiLCJ0IjoiMjlhOTM5YzMtNDJiZC00YmM5LWJiODAtNWE1OTgyNjI2ZTM1IiwicyI6Ik16aGpabUpsTW1NdE1URm1ZUzAwTkRJNExUZzJaR0l0TkdJNU9UYzFOV1JpTkRFeiJ9 
